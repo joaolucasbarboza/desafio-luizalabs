e2988b15f6f29af604be6a7dc53891a4f4284caf
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-08-08 20:14:42 -0300

    feat: criar a classe de implementação de validação do produto com a wishlist.

--- src/main/java/com/luizalabs/wishlist/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/luizalabs/wishlist/service/impl/WishlistServiceImpl.java
@@ -1,12 +1,17 @@
 package com.luizalabs.wishlist.service.impl;
-
+import com.luizalabs.wishlist.entity.ProductEntity;
 import com.luizalabs.wishlist.entity.WishlistEntity;
+import com.luizalabs.wishlist.exceptions.ExistsProductOnWishlist;
+import com.luizalabs.wishlist.exceptions.ProductNotFoundException;
+import com.luizalabs.wishlist.exceptions.WishlistNotFoundException;
 import com.luizalabs.wishlist.repository.ProductRepository;
 import com.luizalabs.wishlist.repository.WishlistRepository;
-import com.luizalabs.wishlist.request.WishlistRequestDTO;
+import com.luizalabs.wishlist.service.ValidationWishlist;
 import com.luizalabs.wishlist.service.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishlistServiceImpl implements WishlistService {
@@ -17,19 +22,28 @@ public class WishlistServiceImpl implements WishlistService {
     @Autowired
     ProductRepository productRepository;
 
-    WishlistEntity wishlistEntity;
-
+    @Autowired
+    private List<ValidationWishlist> validationWishlists;
 
     @Override
-    public WishlistEntity add(WishlistRequestDTO wishlistRequestDTO) {
+    public WishlistEntity add(String productId) {
 
-        var product = productRepository.findById(wishlistRequestDTO.productId())
-                        .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        var product = productRepository.findById(productId)
+                .orElseThrow(ProductNotFoundException::new);
 
-        System.out.println(product);
+        var wishlist = wishlistRepository.findById("66b4eefff40c3c8dbd7f50ef")
+                .orElseThrow(WishlistNotFoundException::new);
 
-//        wishlistEntity.setProductsId();
+        for (ProductEntity prod : wishlist.getProductsId()) {
+            if (prod.getId().equals(productId)) {
+                throw new ExistsProductOnWishlist();
+            }
+        }
 
-        return null;
+        validationWishlists.forEach(v -> v.valid(wishlist));
+
+        wishlist.getProductsId().add(product);
+
+        return this.wishlistRepository.save(wishlist);
     }
 }
